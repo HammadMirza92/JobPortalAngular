bc258952bb505536dbf5606f2f9c3abf67011caf
Hammad Hassan <hammad.hassan@purelogics.com>
2023-05-16 13:04:56 +0500

    newInit

---
+++ src/app/pages/dashboards/candidate-dashboard/candidate-dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CandidateService } from 'src/app/appServices/candidate/candidate.service';
+import { EmployerServiceService } from 'src/app/appServices/employer/employer-service.service';
+import { SecurityService } from 'src/app/appServices/security/security.service';
+
+@Component({
+  selector: 'app-candidate-dashboard',
+  templateUrl: './candidate-dashboard.component.html',
+  styleUrls: ['./candidate-dashboard.component.css']
+})
+export class CandidateDashboardComponent {
+  isAuthenticated = false;
+  candidateId:any;
+  candidateById:any;
+
+
+  constructor(private Security:SecurityService,private candidateService:CandidateService) {
+    this.candidateId = this.Security.getCurrentCandidateId();
+    candidateService.fetchCandidateById(this.candidateId).subscribe((data) => this.candidateById =data);
+  }
+  logout(){
+    debugger;
+    this.Security.logout();
+  }
+}
